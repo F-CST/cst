6881c8da4d36961f43dc85fc13613445e7e0c1c5
saeed saffari <www.44web.ir@gmail.com>
2025-11-07 17:58:56 +0330

    sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,13 @@
-// نسخه ساده و بی‌خطر
-const CACHE_NAME = 'cst-static-v1';
+const CACHE_NAME = 'cst-pwa-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
+  '/cst/',
+  '/cst/index.html',
+  '/cst/manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
   );
 });
